00d51e31f5763c813e518ea2a4df37fae11b3303
ayse3422 <aysemadranesm@gmail.com>
2021-02-21 14:13:11 +0300

    first commit

---
+++ WebApplication12/Controllers/SalonController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication12.Models.Entity;
+
+namespace WebApplication12.Controllers
+{
+    public class SalonController : Controller
+    {
+        // GET: Salon
+        HEALTHYFITEntities db = new HEALTHYFITEntities();
+        public ActionResult SalonKurslarıListele()
+        {
+            var deger = db.SalonKurslariListele.ToList();
+            return View(deger);
+        }
+        public ActionResult SalonListele()
+        {
+            var deger = db.SalonListele.ToList();
+            return View(deger);
+        }
+    }
+}
